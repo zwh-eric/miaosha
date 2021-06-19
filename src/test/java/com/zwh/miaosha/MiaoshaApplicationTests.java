29e07d356f6342ff84f54120f516d750722bfa32
zwh-eric <1092601650@qq.com>
2021-06-19 14:56:21 +0800

    Initial commit

---
+++ src/test/java/com/zwh/miaosha/MiaoshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwh.miaosha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaoshaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
